3dff85fa79ae807fead87846c1b163340964dacb
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2021-07-18 11:28:58 +0500

    saw

--- src/Screens/MainPageScreen.js
+++ src/Screens/MainPageScreen.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import AccordionComponent from "../Components/AccordionComponent";
-import CitySlideComponent from "../Components/CitySlideComponent";
-import {Container, Row, Col} from 'react-bootstrap';
+import AccordionComponent from '../Components/AccordionComponent'
+import CitySlideComponent from '../Components/CitySlideComponent'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const MainPageScreen = () => {
     return (
         <div>
             <Container>
-                <Row className={"mt-5"}>
-                    {[...new Array(4)].map((index) => (
-                    <Col md={6} key={index}>
-                        <AccordionComponent/>
-                    </Col>
-                ))}
+                <Row className={'mt-5'}>
+                    {[...new Array(4)].map(index => (
+                        <Col md={6} key={index}>
+                            <AccordionComponent />
+                        </Col>
+                    ))}
                 </Row>
-            {/*country carousel*/}
-            <CitySlideComponent/>
+                {/*country carousel*/}
+                <CitySlideComponent />
             </Container>
         </div>
     )
-};
+}
 
 export default MainPageScreen
