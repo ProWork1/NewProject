3dadd21cfc78c38dd5606f59a5f2c2d952d89bc5
Mirzamurod <rahimberdiyevmirzamurod@gmail.com>
2021-07-21 12:17:16 +0500

    Navbar azgina ishi qoldi

--- src/Screens/MainPageScreen.js
+++ src/Screens/MainPageScreen.js
@@ -2,23 +2,29 @@ import React from 'react'
 import AccordionComponent from '../Components/AccordionComponent'
 import CitySlideComponent from '../Components/CitySlideComponent'
 import { Container, Row, Col } from 'react-bootstrap'
+import CardUI from './../Components/CardUI'
 
 const MainPageScreen = () => {
     return (
         <div>
             <Container>
-                <Row className={'mt-5'}>
-                    {[...new Array(4)].map(index => (
-                        <Col md={6} key={index}>
-                            <AccordionComponent />
-                        </Col>
-                    ))}
-                </Row>
+                <div className='cardui mb-3' style={{}}>
+                    <CardUI />
+                </div>
+                <div>
+                    <Row className={'mt-5'}>
+                        {[...new Array(4)].map(index => (
+                            <Col md={6} key={index}>
+                                <AccordionComponent />
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
                 {/*country carousel*/}
                 <CitySlideComponent />
             </Container>
         </div>
     )
-};
+}
 
 export default MainPageScreen
